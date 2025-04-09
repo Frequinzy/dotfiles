0e7a7aebc0fa8f075a3dc212c3426b1fc948401e
Emil Hultcrantz <emilhtz@gmail.com>
2025-04-09 15:07:30 +0200

    Neovim: Update telescope bindings and add telescope-undo.nvim

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -14,6 +14,12 @@ return {
     },
     keys = {
         {"<leader>ff", function() require("telescope.builtin").find_files() end,
-        desc="Find files"},
+            desc="Find files"},
+        {"<leader>fg", function() require("telescope.builtin").live_grep() end,
+            desc="Rip grep in project"},
+        {"<leader>fw", function() require("telescope.builtin").grep_string() end,
+            desc="Search for word under cursor"},
+        {"<leader>fh", function() require("telescope.builtin").help_tags() end,
+            desc="Find help tags"},
     },
 }
