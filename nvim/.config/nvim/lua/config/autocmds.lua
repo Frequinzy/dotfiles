c619db1167f26492b28ec3ccfe07417ee813e1ae
Emil Hultcrantz <emilhtz@gmail.com>
2025-07-28 15:37:59 +0200

    Neovim: Add automatic removal of trailing whitespace

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,1 +1,7 @@
--- None currently
+-- Remove trailing whitespace on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*",
+    callback = function()
+        vim.cmd([[silent! keepjumps keeppatterns %s/\s\+$//e]])
+    end,
+})
