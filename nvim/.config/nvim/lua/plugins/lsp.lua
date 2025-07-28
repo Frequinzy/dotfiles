fdff45834448dca03bf7170943ecbc54653765b3
Emil Hultcrantz <emilhtz@gmail.com>
2025-07-28 15:44:00 +0200

    Neovim: Update LSP keybinds

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -14,15 +14,19 @@ return {
         local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 
         local on_attach = function(client, bufnr)
-            local function keybind(...)
-                vim.api.nvim_buf_set_keymap(bufnr, ...)
-            end
-            local opts = { noremap = true, silent = true }
+            local opts = { buffer=bufnr, noremap = true, silent = true }
 
-            keybind("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-            keybind("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-            keybind("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-            keybind("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+            vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+            vim.keymap.set("n", "<leader>qe", function()
+                vim.diagnostic.setqflist({
+                    severity = vim.diagnostic.severity.ERROR,
+                    open = false,
+                    namespace = nil,
+                })
+            end, { noremap = true, silent = true })
         end
 
         require("mason-lspconfig").setup_handlers {
