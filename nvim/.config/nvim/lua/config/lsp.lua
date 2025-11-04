3df423e92217adbea829186bb4f9cb17fb95d51d
Emil Hultcrantz <emilhtz@gmail.com>
2025-11-04 13:26:35 +0100

    Neovim: Swtich to Blink for completion

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -10,10 +10,9 @@ vim.api.nvim_create_autocmd('LspAttach', {
     group = vim.api.nvim_create_augroup('lsp', {}),
     callback = function(args)
         local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-        -- Autocomplete
         if client:supports_method('textDocument/completion') then
-            vim.opt.completeopt = { 'fuzzy', 'menu', 'menuone', 'noinsert', 'popup' }
-            vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+            vim.opt.completeopt = { 'fuzzy', 'menu', 'menuone', 'popup' }
+            vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = false })
         end
     end
 })
